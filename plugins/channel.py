6f8bb0c6b41594c356ae5ad5c5b5c1476db43320
gbrohit <119616499+gbrohit@users.noreply.github.com>
2026-09-02 21:43:26 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -3,14 +3,18 @@
 # Ask Doubt on telegram @KingVJ01
 
 from pyrogram import Client, filters
-from info import CHANNELS
-from database.ia_filterdb import save_file
+from info import REQ_CHANNEL
+from database.join_reqs import JoinReqs
 
-media_filter = filters.document | filters.video
+join_db = JoinReqs()
 
-@Client.on_message(filters.chat(CHANNELS) & media_filter)
-async def media(bot, message):
-    media = getattr(message, message.media.value, None)
-    media.caption = message.caption
-    await save_file(media)
-
+@Client.on_chat_join_request(filters.chat(REQ_CHANNEL) if REQ_CHANNEL else None)
+async def req_channel_handler(client, message):
+    if not join_db.isActive():
+        return
+    await join_db.add_user(
+        user_id=message.from_user.id,
+        first_name=message.from_user.first_name,
+        username=message.from_user.username,
+        date=message.date
+    )
