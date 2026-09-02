b62f6fe0625b4c27f890ab96047d181fc6e4c4ff
gbrohit <119616499+gbrohit@users.noreply.github.com>
2026-09-02 22:04:53 +0530

    Update join_reqs.py

--- database/join_reqs.py
+++ database/join_reqs.py
@@ -3,44 +3,41 @@
 # Ask Doubt on telegram @KingVJ01
 
 import motor.motor_asyncio
-from info import REQ_CHANNEL, OTHER_DB_URI
+from info import REQ_CHANNEL, OTHER_DB_URI, DATABASE_URI
 
 class JoinReqs:
-
     def __init__(self):
-        if OTHER_DB_URI:
-            self.client = motor.motor_asyncio.AsyncIOMotorClient(OTHER_DB_URI)
+        uri = OTHER_DB_URI or DATABASE_URI
+        if uri:
+            self.client = motor.motor_asyncio.AsyncIOMotorClient(uri)
             self.db = self.client["JoinReqs"]
             self.col = self.db[str(REQ_CHANNEL)]
         else:
             self.client = None
-            self.db = None
-            self.col = None
 
     def isActive(self):
-        if self.client is not None:
-            return True
-        else:
-            return False
+        return True if self.client else not not DATABASE_URI
 
     async def add_user(self, user_id, first_name, username, date):
-        try:
-            await self.col.insert_one({"_id": int(user_id),"user_id": int(user_id), "first_name": first_name, "username": username, "date": date})
-        except:
-            pass
+        if not self.isActive():
+            return
+        await self.col.update_one(
+            {"user_id": user_id},
+            {"$set": {"first_name": first_name, "username": username, "date": date}},
+            upsert=True
+        )
 
     async def get_user(self, user_id):
-        return await self.col.find_one({"user_id": int(user_id)})
-
-    async def get_all_users(self):
-        return await self.col.find().to_list(None)
-
-    async def delete_user(self, user_id):
-        await self.col.delete_one({"user_id": int(user_id)})
-
-    async def delete_all_users(self):
-        await self.col.delete_many({})
+        if not self.isActive():
+            return None
+        return await self.col.find_one({"user_id": user_id})
 
     async def get_all_users_count(self):
+        if not self.isActive():
+            return 0
         return await self.col.count_documents({})
 
+    async def delete_all_users(self):
+        if not self.isActive():
+            return
+        await self.col.drop()
