3a41c83ef4162c52697dfc0505a01d25af156dca
gbrohit <119616499+gbrohit@users.noreply.github.com>
2026-09-04 03:29:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -214,7 +214,7 @@ async def start(client, message):
         file_id = data
         pre = ""
 
-    elif data.split("-", 1)[0] == "verify":
+    if data.split("-", 1)[0] == "verify":
         userid = data.split("-", 2)[1]
         token = data.split("-", 3)[2]
         if str(message.from_user.id) != str(userid):
