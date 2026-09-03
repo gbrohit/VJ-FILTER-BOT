f0a70d1481fd94ccdc0d82d8f69fb98872df6e90
gbrohit <119616499+gbrohit@users.noreply.github.com>
2026-09-04 02:51:09 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,9 +1,4 @@
-# Don't Remove Credit @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 from aiohttp import web
-from .route import routes
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
