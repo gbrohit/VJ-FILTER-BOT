e902ba7017a1ff7e97e58a93b42fb5c04c8ab53d
gbrohit <119616499+gbrohit@users.noreply.github.com>
2026-09-04 03:06:18 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- plugins/commands.py
+++ plugins/commands.py
@@ -2,14 +2,24 @@ import os, string, logging, random, asyncio, time, datetime, re, sys, json, base
 from Script import script
 from pyrogram import Client, filters, enums
 from pyrogram.errors import ChatAdminRequired, FloodWait
-from pyrogram.types import *
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from database.ia_filterdb import col, sec_col, get_file_details, unpack_new_file_id, get_bad_files
 from database.users_chats_db import db, delete_all_referal_users, get_referal_users_count, get_referal_all_users, referal_add_user
-from info import CLONE_MODE, OWNER_LNK, REACTIONS, CHANNELS, REQUEST_TO_JOIN_MODE, TRY_AGAIN_BTN, ADMINS, SHORTLINK_MODE, PREMIUM_AND_REFERAL_MODE, STREAM_MODE, AUTH_CHANNEL, REFERAL_PREMEIUM_TIME, REFERAL_COUNT, PAYMENT_TEXT, PAYMENT_QR, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT, CHNL_LNK, GRP_LNK, REQST_CHANNEL, SUPPORT_CHAT, MAX_B_TN, VERIFY, SHORTLINK_API, SHORTLINK_URL, TUTORIAL, VERIFY_TUTORIAL, IS_TUTORIAL, URL
-from utils import get_settings, pub_is_subscribed, get_size, is_subscribed, save_group_settings, temp, verify_user, check_token, check_verification, get_token, get_shortlink, get_tutorial, get_seconds
+from info import (
+    OWNER_LNK, REACTIONS, CHANNELS, ADMINS, SHORTLINK_MODE, 
+    PREMIUM_AND_REFERAL_MODE, AUTH_CHANNEL, REFERAL_PREMEIUM_TIME, 
+    REFERAL_COUNT, PAYMENT_TEXT, PAYMENT_QR, LOG_CHANNEL, PICS, 
+    CUSTOM_FILE_CAPTION, PROTECT_CONTENT, CHNL_LNK, GRP_LNK, 
+    REQST_CHANNEL, SUPPORT_CHAT, MAX_B_TN, VERIFY, SHORTLINK_API, 
+    SHORTLINK_URL, TUTORIAL, VERIFY_TUTORIAL, IS_TUTORIAL
+)
+from utils import (
+    get_settings, pub_is_subscribed, get_size, is_subscribed, 
+    save_group_settings, temp, verify_user, check_token, 
+    check_verification, get_token, get_shortlink, get_tutorial, get_seconds
+)
 from database.connections_mdb import active_connection
-from urllib.parse import quote_plus
-from TechVJ.util.file_properties import get_name, get_hash, get_media_file_size
+
 logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
