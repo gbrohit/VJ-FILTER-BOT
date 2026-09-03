72008545f0058b3683819de0de86408e60fdbcac
gbrohit <119616499+gbrohit@users.noreply.github.com>
2026-09-04 01:30:19 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -231,39 +231,3 @@ Aᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅs:
     ▀█▀ █░█ █▀▀ █ █▄░█ █▀ █▀█ █▀▄▀█ █▄░█ █ ▄▀█ █▀▀ █▀ █▀▀ █░░ █░█ █▄▄
     ░█░ █▀█ ██▄ █ █░▀█ ▄█ █▄█ █░▀░█ █░▀█ █ █▀█ █▄▄ ▄█ █▄▄ █▄▄ █▄█ █▄█
     """
- 
-
-    RENAME_TXT = """
-🌌 <b><u>HOW TO SET THUMBNAIL</u></b>
-  
-•> /set_thumb - send any picture to automatically set thumbnail.
-•> /del_thumb use this command and delete your old thumbnail.
-•> /view_thumb use this command view your current thumbnail.
-
-📑 <b><u>HOW TO SET CUSTOM CAPTION</u></b>
-
-•> /set_caption - set a custom caption
-•> /see_caption - see your custom caption
-•> /del_caption - delete custom caption
-
-Example:- /set_caption 📕 File Name: {filename}
-💾 Size: {filesize}
-⏰ Duration: {duration}
-
-✏️ <b><u>HOW TO RENAME A FILE</u></b>
-
-•> /rename - send any file and click rename option and type new file name and \nthen select [ document, video, audio ]👈 choice this.
-"""
-
-    STREAM_TXT = """<b><u>HOW TO GET STREAM AND DOWNLOAD LINK :</u>
-
-/stream - ɢᴇᴛ sᴛʀᴇᴀᴍᴀʙʟᴇ ᴀɴᴅ ᴅᴏᴡɴʟᴏᴀᴅᴀʙʟᴇ ʟɪɴᴋ ᴏғ ᴀɴʏ ғɪʟᴇ</b>"""
-
-
-# Don't Remove Credit @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-
-    
-
